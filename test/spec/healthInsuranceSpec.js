917356a4c37082a3fb7914e75b1e449907de858b
janpapenbrock <j.papenbrock@hochzehn.com>
2017-01-02 13:55:51 +0100

    Re-structure and extend tests.

--- test/spec/healthInsuranceSpec.js
+++ test/spec/healthInsuranceSpec.js
@@ -3,34 +3,67 @@ describe("healthInsurance", function () {
 
     const MINIMUM_INCOME = 2231.25;
 
-    it("should calculate health insurance contribution for minimum income", function () {
-        var result = subject({
-            income: MINIMUM_INCOME,
-            age: 25,
-            hasChildren: false,
-            insuranceCompany: 'tk'
+    describe("when earning minimum income", function() {
+
+        it("should calculate health insurance contribution without sick pay", function () {
+            var result = subject({
+                income: MINIMUM_INCOME,
+                age: 25,
+                hasChildren: false,
+                insuranceCompany: 'tk'
+            });
+
+            expect(result).toEqual({
+                totalContribution: 397.17,
+                nursingCareInsuranceContribution: 62.48,
+                healthInsuranceContribution: 334.69
+            });
         });
 
-        expect(result).toEqual({
-            totalContribution: 397.17,
-            nursingCareInsuranceContribution: 62.48,
-            healthInsuranceContribution: 334.69
-        });
-    });
+        it("should calculate health insurance contribution with sick pay", function () {
+            var result = subject({
+                income: MINIMUM_INCOME,
+                age: 25,
+                hasChildren: false,
+                withSickPay: true,
+                insuranceCompany: 'tk'
+            });
 
-    it("should calculate health insurance contribution for minimum income", function () {
-        var result = subject({
-            income: MINIMUM_INCOME,
-            age: 25,
-            hasChildren: false,
-            withSickPay: true,
-            insuranceCompany: 'tk'
+            expect(result).toEqual({
+                totalContribution: 410.55,
+                nursingCareInsuranceContribution: 62.48,
+                healthInsuranceContribution: 348.07
+            });
         });
 
-        expect(result).toEqual({
-            totalContribution: 410.55,
-            nursingCareInsuranceContribution: 62.48,
-            healthInsuranceContribution: 348.07
+        it("should calculate health insurance contribution with children", function () {
+            var result = subject({
+                income: MINIMUM_INCOME,
+                age: 25,
+                hasChildren: true,
+                insuranceCompany: 'tk'
+            });
+
+            expect(result).toEqual({
+                totalContribution: 391.59,
+                nursingCareInsuranceContribution: 56.90,
+                healthInsuranceContribution: 334.69
+            });
+        });
+
+        it("should calculate health insurance contribution younger than 23", function () {
+            var result = subject({
+                income: MINIMUM_INCOME,
+                age: 22,
+                hasChildren: false,
+                insuranceCompany: 'tk'
+            });
+
+            expect(result).toEqual({
+                totalContribution: 391.59,
+                nursingCareInsuranceContribution: 56.90,
+                healthInsuranceContribution: 334.69
+            });
         });
     });
 
